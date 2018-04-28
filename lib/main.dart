e1a7185a3c53e8ac8d1a74c6aae97ebd42b449e5
Benjamin Acquah <fiifibenjamindeveloper@gmail.com>
2018-04-28 23:13:51 +0000

    designed the walkthrough page assets or icons are absent

--- lib/main.dart
+++ lib/main.dart
@@ -1,109 +1,130 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:udl/page_dragger.dart';
+import 'package:udl/page_reveal.dart';
+import 'package:udl/pager_indicator.dart';
+import 'package:udl/pages.dart';
 
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: 'Flutter Demo',
+      title: 'UDL',
       theme: new ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
-        // counter didn't reset back to zero; the application is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: new MyHomePage(title: 'Flutter Demo Home Page'),
+      home: new MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
 
   @override
   _MyHomePageState createState() => new _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  StreamController<SlideUpdate> slideUpdateStream;
+  AnimatedPageDragger animatedPageDragger;
+
+  int activeIndex = 0;
+  int nextPageIndex = 0;
+  SlideDirection slideDirection = SlideDirection.none;
+  double slidePercent = 0.0;
+
+  _MyHomePageState() {
+    slideUpdateStream = new StreamController<SlideUpdate>();
+
+    slideUpdateStream.stream.listen((SlideUpdate event) {
+      setState(() {
+        if (event.updateType == UpdateType.dragging) {
+          print('Sliding ${event.direction} at ${event.slidePercent}');
+          slideDirection = event.direction;
+          slidePercent = event.slidePercent;
+
+          if (slideDirection == SlideDirection.leftToRight) {
+            nextPageIndex = activeIndex - 1;
+          } else if (slideDirection == SlideDirection.rightToLeft) {
+            nextPageIndex = activeIndex + 1;
+          } else {
+            nextPageIndex = activeIndex;
+          }
+        } else if (event.updateType == UpdateType.doneDragging) {
+          print('Done dragging.');
+          if (slidePercent > 0.5) {
+            animatedPageDragger = new AnimatedPageDragger(
+              slideDirection: slideDirection,
+              transitionGoal: TransitionGoal.open,
+              slidePercent: slidePercent,
+              slideUpdateStream: slideUpdateStream,
+              vsync: this,
+            );
+          } else {
+            animatedPageDragger = new AnimatedPageDragger(
+              slideDirection: slideDirection,
+              transitionGoal: TransitionGoal.close,
+              slidePercent: slidePercent,
+              slideUpdateStream: slideUpdateStream,
+              vsync: this,
+            );
+
+            nextPageIndex = activeIndex;
+          }
+
+          animatedPageDragger.run();
+        } else if (event.updateType == UpdateType.animating) {
+          print('Sliding ${event.direction} at ${event.slidePercent}');
+          slideDirection = event.direction;
+          slidePercent = event.slidePercent;
+        } else if (event.updateType == UpdateType.doneAnimating) {
+          print('Done animating. Next page index: $nextPageIndex');
+          activeIndex = nextPageIndex;
+
+          slideDirection = SlideDirection.none;
+          slidePercent = 0.0;
+
+          animatedPageDragger.dispose();
+        }
+      });
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return new Scaffold(
-      appBar: new AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: new Text(widget.title),
-      ),
-      body: new Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: new Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug paint" (press "p" in the console where you ran
-          // "flutter run", or select "Toggle Debug Paint" from the Flutter tool
-          // window in IntelliJ) to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            new Text(
-              'You have pushed the button this many times:',
+      body: new Stack(
+        children: [
+          new Page(
+            viewModel: pages[activeIndex],
+            percentVisible: 1.0,
+          ),
+          new PageReveal(
+            revealPercent: slidePercent,
+            child: new Page(
+              viewModel: pages[nextPageIndex],
+              percentVisible: slidePercent,
             ),
-            new Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
+          ),
+          new PagerIndicator(
+            viewModel: new PagerIndicatorViewModel(
+              pages,
+              activeIndex,
+              slideDirection,
+              slidePercent,
             ),
-          ],
-        ),
+          ),
+          new PageDragger(
+            canDragLeftToRight: activeIndex > 0,
+            canDragRightToLeft: activeIndex < pages.length - 1,
+            slideUpdateStream: this.slideUpdateStream,
+          ),
+        ],
       ),
-      floatingActionButton: new FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: new Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
---
+++ lib/pages.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+
+final pages = [
+  new PageViewModel(
+      const Color(0xFF678FB4),
+      '../assets/',
+      'Engagement',
+      'The Why Of Learning',
+      '../assets/'
+  ),
+  new PageViewModel(
+      const Color(0xFF65B0B4),
+      '../assets/',
+      'Representation',
+      'The What Of Leraning',
+      '../assets/'
+  ),
+  new PageViewModel(
+    const Color(0xFF9B90BC),
+    '../assets/',
+    'Action & Expression',
+    'The How Of Learning',
+    '../assets/',
+  ),
+];
+
+class Page extends StatelessWidget {
+
+  final PageViewModel viewModel;
+  final double percentVisible;
+
+  Page({
+    this.viewModel,
+    this.percentVisible = 1.0,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+        width: double.INFINITY,
+        color: viewModel.color,
+        child: new Opacity(
+          opacity: percentVisible,
+          child: new Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                new Transform(
+                  transform: new Matrix4.translationValues(0.0, 50.0 * (1.0 - percentVisible), 0.0),
+                  child: new Padding(
+                    padding: new EdgeInsets.only(bottom: 25.0),
+                    child: new Image.asset(
+                        viewModel.heroAssetPath,
+                        width: 200.0,
+                        height: 200.0
+                    ),
+                  ),
+                ),
+                new Transform(
+                  transform: new Matrix4.translationValues(0.0, 30.0 * (1.0 - percentVisible), 0.0),
+                  child: new Padding(
+                    padding: new EdgeInsets.only(top: 10.0, bottom: 10.0),
+                    child: new Text(
+                      viewModel.title,
+                      style: new TextStyle(
+                        color: Colors.white,
+                        fontFamily: 'FlamanteRoma',
+                        fontSize: 34.0,
+                      ),
+                    ),
+                  ),
+                ),
+                new Transform(
+                  transform: new Matrix4.translationValues(0.0, 30.0 * (1.0 - percentVisible), 0.0),
+                  child: new Padding(
+                    padding: new EdgeInsets.only(bottom: 75.0),
+                    child: new Text(
+                      viewModel.body,
+                      textAlign: TextAlign.center,
+                      style: new TextStyle(
+                        color: Colors.white,
+                        fontSize: 18.0,
+                      ),
+                    ),
+                  ),
+                ),
+              ]
+          ),
+        )
+    );
+  }
+}
+
+class PageViewModel {
+  final Color color;
+  final String heroAssetPath;
+  final String title;
+  final String body;
+  final String iconAssetPath;
+
+  PageViewModel(
+    this.color,
+    this.heroAssetPath,
+    this.title,
+    this.body,
+    this.iconAssetPath,
+  );
+}
